55490c19e2c53bca07e92c6032ce16a02b459edd
Basil El-Hindi <basilelhindi@gmail.com>
2021-05-07 17:30:12 -0500

    Initial commit of portfolio build

---
+++ Assets/Scripts/GameInput.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameInput : MonoBehaviour
+{
+    void Update()
+    {
+        // Backspace resets level
+        if (Input.GetKeyDown(KeyCode.Backspace))
+        {
+            // Determine and reload scene
+            int currentLevelIndex = SceneManager.GetActiveScene().buildIndex;
+            SceneManager.LoadScene(currentLevelIndex);
+        }
+
+        // Escape exits game
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+}
---
+++ Assets/Scripts/KillVolume.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillVolume : MonoBehaviour
+{
+    UIController uIController = null;
+
+    private void Awake()
+    {
+        uIController = FindObjectOfType<UIController>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Turn off game object that enters
+        // other.gameObject.SetActive(false);
+        PlayerController playerController = other.GetComponent<PlayerController>();
+        if (playerController != null)
+        {
+            playerController.Die();
+        }
+
+        if (uIController != null)
+        {
+            uIController.ShowWinText("You Died!\nPress backspace to restart");
+        }
+    }
+}
